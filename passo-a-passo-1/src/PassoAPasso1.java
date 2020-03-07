17e369cf227781fbb932c001fff753c82e2e3708
Ademir Mazer Jr [ Nuno ] <ademir.mazer.jr@gmail.com>
2020-03-07 10:02:57 -0300

    primeira parte do exercício

--- passo-a-passo-1/src/PassoAPasso1.java
+++ passo-a-passo-1/src/PassoAPasso1.java
@@ -8,7 +8,23 @@ public class PassoAPasso1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conta c1 = new Conta();
+
+          c1.titular = "Hugo";
+          c1.num = 123;
+          c1.agencia = "45678-9";
+          c1.saldo = 50.0;
+          c1.dataAbertura = "04/06/2015";
+
+          c1.depositar(100.0);
+          System.out.println("saldo atual:" + c1.saldo);
+          System.out.println("rendimento mensal:" + c1.calcularRendimento());
+
+          c1.sacar(20.0);
+          System.out.println("saldo atual:" + c1.saldo);
+          System.out.println("rendimento mensal:" + c1.calcularRendimento());
+
+          System.out.println("Olá " + c1.titular + " seu saldo atual é de " + c1.saldo);
     }
 
 }
