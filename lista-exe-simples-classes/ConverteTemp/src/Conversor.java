a4ea47664797782c430024f26c3d3ee2fb48acc9
Ademir Mazer Jr [ Nuno ] <ademir.mazer.jr@gmail.com>
2020-03-14 09:03:55 -0300

    corrige cálculo de double

--- lista-exe-simples-classes/ConverteTemp/src/Conversor.java
+++ lista-exe-simples-classes/ConverteTemp/src/Conversor.java
@@ -4,11 +4,11 @@
  */
 public class Conversor {
     public double converterEmCelsius(double F) {
-        return (F - 32) * (5/9);
+        return (F - 32) * (5.0/9.0);
     }
 
     public double converterEmFahrenheit(double C) {
-        return (9 * C + 160) / 5;
+        return (9.0 * C + 160.0) / 5.0;
     }
 
     public double converter(Temperatura t) {
