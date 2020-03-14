391ef56bd28b64886294da9246179d350331e670
Ademir Mazer Jr [ Nuno ] <ademir.mazer.jr@gmail.com>
2020-03-14 09:04:15 -0300

    corrige saída para info

--- lista-exe-simples-classes/ConverteTemp/src/ConverteTemp.java
+++ lista-exe-simples-classes/ConverteTemp/src/ConverteTemp.java
@@ -17,20 +17,20 @@ public class ConverteTemp {
     public static void main(String[] args) {
         Conversor c = new Conversor();
 
-        System.err.println("1C = " + c.converterEmFahrenheit(3) + "F");
-        System.err.println("1F = " + c.converterEmCelsius(1) + "C");
+        System.out.println("1C = " + c.converterEmFahrenheit(3) + "F");
+        System.out.println("1F = " + c.converterEmCelsius(1.0) + "C");
 
-        System.err.println("30C = " + c.converterEmFahrenheit(30) + "F");
-        System.err.println("110F = " + c.converterEmCelsius(110) + "C");
+        System.out.println("30C = " + c.converterEmFahrenheit(30) + "F");
+        System.out.println("110F = " + c.converterEmCelsius(110) + "C");
 
         Temperatura t = new Temperatura();
         t.tipo = 'C'; // note que strings com aspas duplas e char com aspas simples
         t.valor = 91;
 
-        System.err.println("91C = " + c.converter(t) + "F");
+        System.out.println("91C = " + c.converter(t) + "F");
 
         t.tipo = 'F';
-        System.err.println("91F = " + c.converter(t) + "C");
+        System.out.println("91F = " + c.converter(t) + "C");
 
 
     }
